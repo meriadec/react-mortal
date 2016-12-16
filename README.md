32554269e6b838180253196fe343af43b439d5ca
meriadec <meriadec.pillet@gmail.com>
2016-12-16 10:41:11 +0100

    Add basic stuff, and basic doc

---
+++ README.md
@@ -0,0 +1,40 @@
+# react-motionportal
+
+## Usage
+
+```
+import React, { Component } from 'react'
+import MotionPortal from 'react-motionportal'
+
+class AnimatedModal extends Component {
+
+  state = {
+    open: false,
+  }
+
+  handleClose = () => this.setState({ open: false })
+
+  render () {
+    return (
+      <MotionPortal
+        isOpened={this.state.open}
+        motionStyle={(spring, isVisible) => ({
+          opacity: spring(isVisible ? 1 : 0),
+        })}
+      >
+        {(motion, isVisible) => (
+          <div
+            style={{
+              opacity: motion.opacity,
+              pointerEvents: isVisible ? 'auto' : 'none',
+            }}
+          >
+            {'modal content'}
+          </div>
+        )}
+      </MotionPortal>
+    )
+  }
+
+}
+```
---
+++ src/MotionPortal.js
@@ -0,0 +1,122 @@
+import React, { Component, PropTypes } from 'react'
+import { Motion, spring } from 'react-motion'
+import Portal from 'react-portal'
+
+class MotionPortal extends Component {
+
+  static propTypes = {
+    onClose: PropTypes.func.isRequired,
+  }
+
+  state = {
+    portalOpened: false,
+    isVisible: false,
+  }
+
+  componentWillMount () {
+
+    this.handleKey = ::this.handleKey
+    this.handleRest = ::this.handleRest
+    this.handlePortalOpen = ::this.handlePortalOpen
+
+    // if portal is created opened,
+    // no animation is needed
+    if (this.props.isOpened) {
+      this.setState({
+        portalOpened: true,
+        isVisible: true,
+      })
+    }
+
+  }
+
+  componentDidMount () {
+    window.addEventListener('keydown', this.handleKey)
+  }
+
+  componentWillReceiveProps (nextProps) {
+
+    // check if we just opened panel
+
+    const hasOpened = !this.props.isOpened && nextProps.isOpened
+
+    if (hasOpened) {
+      // open portal
+      this.setState({ portalOpened: true })
+    }
+
+    // check if we just closed panel
+
+    const hasClosed = this.props.isOpened && !nextProps.isOpened
+
+    if (hasClosed) {
+      // launch close animation
+      this.setState({ isVisible: false })
+    }
+
+    // check if we re-opened panel while closing
+
+    const hasReopened = hasOpened && !this.state.isVisible && this.state.portalOpened
+
+    if (hasReopened) {
+      // re-launch animation
+      this.setState({ isVisible: true })
+    }
+
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('keydown', this.handleKey)
+  }
+
+  handleKey (e) {
+    // handle ESC key
+    if (e.which === 27 && this.state.portalOpened) {
+      this.props.onClose()
+    }
+  }
+
+  handlePortalOpen () {
+    // portal just open, launch animation
+    this.setState({ isVisible: true })
+  }
+
+  handleRest () {
+    // if panel is not visible, close portal
+    if (!this.state.isVisible) {
+      this.setState({
+        portalOpened: false,
+      })
+    }
+  }
+
+  render () {
+
+    const {
+      motionStyle,
+      children,
+    } = this.props
+
+    const {
+      portalOpened,
+      isVisible,
+    } = this.state
+
+    return (
+      <Portal
+        isOpened={portalOpened}
+        onOpen={this.handlePortalOpen}
+      >
+        <Motion
+          onRest={this.handleRest}
+          style={motionStyle(spring, isVisible)}
+        >
+          {motion => children(motion, isVisible)}
+        </Motion>
+      </Portal>
+    )
+  }
+
+}
+
+export default MotionPortal
