959d1ae42e94625b932053b42a4f0bcd153a1295
meriadec <meriadec.pillet@gmail.com>
2017-10-05 09:50:30 +0200

    deploy

---
+++ Modal/Modal.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+// replace this by
+// import Mortal from 'react-mortal'
+import Mortal from '../../src/Mortal'
+
+class Modal extends Component {
+  render() {
+    const { isOpened, onClose, children } = this.props
+
+    return (
+      <Mortal
+        isOpened={isOpened}
+        onClose={onClose}
+        motionStyle={(spring, isVisible) => ({
+          opacity: spring(isVisible ? 1 : 0),
+          modalOffset: spring(isVisible ? 0 : -90, {
+            stiffness: isVisible ? 300 : 200,
+            damping: isVisible ? 15 : 30,
+          }),
+        })}
+      >
+        {(motion, isVisible) => (
+          <div
+            className="Modal"
+            style={{
+              pointerEvents: isVisible ? 'auto' : 'none',
+            }}
+          >
+            <div
+              className="Modal--overlay"
+              onClick={onClose}
+              style={{
+                opacity: motion.opacity,
+                pointerEvents: isVisible ? 'auto' : 'none',
+              }}
+            />
+            <div
+              className="Modal--body"
+              style={{
+                opacity: motion.opacity,
+                transform: `translate3d(0, ${motion.modalOffset}px, 0)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        )}
+      </Mortal>
+    )
+  }
+}
+
+export default Modal
---
+++ Panel/Panel.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+// replace this by
+// import Mortal from 'react-mortal'
+import Mortal from '../../src/Mortal'
+
+class Panel extends Component {
+  render() {
+    const { isOpened, onClose, children } = this.props
+
+    return (
+      <Mortal
+        isOpened={isOpened}
+        onClose={onClose}
+        motionStyle={(spring, isVisible) => ({
+          opacity: spring(isVisible ? 1 : 0),
+          panelOffset: spring(isVisible ? 0 : 100),
+        })}
+      >
+        {(motion, isVisible) => (
+          <div
+            className="Panel"
+            style={{
+              pointerEvents: isVisible ? 'auto' : 'none',
+            }}
+          >
+            <div
+              className="Panel--overlay"
+              onClick={onClose}
+              style={{
+                opacity: motion.opacity,
+                pointerEvents: isVisible ? 'auto' : 'none',
+              }}
+            />
+            <div
+              className="Panel--body"
+              style={{
+                transform: `translate3d(${motion.panelOffset}%, 0, 0)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        )}
+      </Mortal>
+    )
+  }
+}
+
+export default Panel
